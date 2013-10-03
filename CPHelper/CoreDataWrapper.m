87cd60f5bfadfb10bb1c66224e926eb8cc09e552
Collin Price <collin@collinprice.com>
2013-10-03 12:07:21 -0400

    Fixed core data wrapper models name.

--- CPHelper/CoreDataWrapper.m
+++ CPHelper/CoreDataWrapper.m
@@ -80,7 +80,7 @@ NSString * const DataManagerDidSaveFailedNotification = @"DataManagerDidSaveFail
     if (_managedObjectModel != nil) {
         return _managedObjectModel;
     }
-    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"Models" withExtension:@"momd"];
+    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:[BundleHelper bundleName] withExtension:@"momd"];
     _managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
     return _managedObjectModel;
 }
